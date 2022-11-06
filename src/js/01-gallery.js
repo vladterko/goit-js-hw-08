8eab13db4b5a048193af1fb4eea5d8594bcdcbc7
Vlad Tereshchenko <vlad.terko@gmail.com>
2022-11-06 22:14:07 +0200

    Adds SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,17 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-console.log('Helo world!');
-console.log('test');
+const galleryContent = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({preview, original, description}) => `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+    ).join('');
+
+galleryContent.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
