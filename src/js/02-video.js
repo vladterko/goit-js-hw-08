ca5a80b8b96eb9002a2afec4f308dd8d54377cbe
Vlad Tereshchenko <vlad.terko@gmail.com>
2022-11-10 23:57:14 +0200

    Adds task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME = "videoplayer-current-time";
+
+const onPlay = function (data) {
+    localStorage.setItem(CURRENT_TIME, JSON.stringify(data.seconds));
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
